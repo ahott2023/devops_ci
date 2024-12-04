fe52303fd74b9b17d5e0ec267fd9b81373030a98
ahott2023 <ahott2016@gmail.com>
2024-12-03 22:24:07 -0300

    Add test modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -10,7 +10,7 @@ public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio 11";
+       String message = "Cambio en el mensaje de prueba";
         assertNotNull(message);
     }
 }
